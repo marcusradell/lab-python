f319e7fa35ef1bd6afb467cfdcd2dee23538c8f4
Marcus Rådell <marcus@radell.net>
2025-10-28 16:44:30 +0100

    load the consumed json event

--- src/lab_python/live_subtitler.py
+++ src/lab_python/live_subtitler.py
@@ -1,3 +1,4 @@
+import json
 from confluent_kafka import Consumer
 
 
@@ -28,10 +29,18 @@ def liveSubtitler() -> None:
 
             if message_value is None:
                 print(f"Consumed empty event from topic {message_topic}.")
+                continue
+
+            message_payload = json.loads(message_value)
+            if message_payload["cmd"] == "start":
+                print(
+                    f"Consumed event -> topic: {message_topic}, channel: {message_payload['channel']}, program_id: {message_payload['program_id']}"
+                )
             else:
                 print(
-                    f"Consumed event from topic {message_topic}, value = {message_value.decode('utf-8')}"
+                    f"Consumed unhandled event -> topic: {message_topic}, value: {message_value.decode('utf-8')}"
                 )
+
     except KeyboardInterrupt:
         pass
     finally:
