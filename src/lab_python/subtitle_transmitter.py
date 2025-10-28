8aa9d08badd29ee8e2c270d0924b132d4f388e91
Marcus Rådell <marcus@radell.net>
2025-10-28 13:46:23 +0100

    produce dummy start message

--- src/lab_python/subtitle_transmitter.py
+++ src/lab_python/subtitle_transmitter.py
@@ -1,9 +1,35 @@
+import json
+import time
+from confluent_kafka import Producer
+from typing import TypedDict
+
+Message = TypedDict("Message", {"type": str, "timestamp": float})
+
+
 class SubtitleTransmitter:
     def __init__(self) -> None:
-        pass
+        self.kafka_producer_configuration = {
+            "bootstrap.servers": "localhost:9092",
+            "client.id": "python-producer",
+        }
+
+        self.producer = Producer(self.kafka_producer_configuration)
 
     def start(self) -> None:
-        pass
+        message: Message = {
+            "type": "START",
+            "timestamp": time.time(),
+        }
+
+        message_bytes = json.dumps(message).encode("utf-8")
+
+        self.producer.produce(
+            topic="subtitle_transmitter_actions",
+            value=message_bytes,
+            callback=lambda err, msg: print(f"err: {err} msg: {msg}"),
+        )
+
+        self.producer.flush()
 
     def stop(self) -> None:
         pass
